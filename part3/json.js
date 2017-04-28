02043ab54f4583cbbe79c0d50a04669373fc334f
Vlad Pasculescu <vpasculescu@Vlads-MacBook-Pro.local>
2017-04-27 20:48:41 -0700

    Commit 2

---
+++ part3/json.js
@@ -0,0 +1,17 @@
+{"teammembers":[
+  {
+    "name":"Vlad Pasculescu",
+    "title":"Jortsman",
+    "bio":"Hashtag cray banh mi, kombucha trust fund waistcoat snackwave. Hashtag food truck swag woke. Cold-pressed 90's semiotics, gluten-free fashion axe vexillologist chicharrones cray kombucha hammock offal locavore helvetica pug retro. Bitters dreamcatcher ramps humblebrag, pinterest cold-pressed kale chips chambray you probably haven't heard of them fanny pack chillwave bicycle rights. Tacos VHS la croix, twee wayfarers helvetica iPhone cliche letterpress echo park DIY schlitz cardigan everyday carry. Next level tilde meh williamsburg. Dreamcatcher tote bag green juice, post-ironic next level brunch bitters meggings chicharrones literally readymade fam."
+  },
+  {
+    "name":"Dana Pasculescu",
+    "title":"Wife",
+    "bio":"Hashtag cray banh mi, kombucha trust fund waistcoat snackwave. Hashtag food truck swag woke. Cold-pressed 90's semiotics, gluten-free fashion axe vexillologist chicharrones cray kombucha hammock offal locavore helvetica pug retro. Bitters dreamcatcher ramps humblebrag, pinterest cold-pressed kale chips chambray you probably haven't heard of them fanny pack chillwave bicycle rights. Tacos VHS la croix, twee wayfarers helvetica iPhone cliche letterpress echo park DIY schlitz cardigan everyday carry. Next level tilde meh williamsburg. Dreamcatcher tote bag green juice, post-ironic next level brunch bitters meggings chicharrones literally readymade fam."
+  },
+  {
+    "name":"Baxter",
+    "title":"Child-Dog",
+    "bio":"Hashtag cray banh mi, kombucha trust fund waistcoat snackwave. Hashtag food truck swag woke. Cold-pressed 90's semiotics, gluten-free fashion axe vexillologist chicharrones cray kombucha hammock offal locavore helvetica pug retro. Bitters dreamcatcher ramps humblebrag, pinterest cold-pressed kale chips chambray you probably haven't heard of them fanny pack chillwave bicycle rights. Tacos VHS la croix, twee wayfarers helvetica iPhone cliche letterpress echo park DIY schlitz cardigan everyday carry. Next level tilde meh williamsburg. Dreamcatcher tote bag green juice, post-ironic next level brunch bitters meggings chicharrones literally readymade fam."
+  }
+]}
